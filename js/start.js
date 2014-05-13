f4823e6a3bb9facd023126f27a5912e1b4222b72
xero <x@xero.nu>
2014-05-13 15:32:57 -0400

    add minified version of style to speed up rendering a little.

--- js/start.js
+++ js/start.js
@@ -1,4 +1,4 @@
 var link = document.createElement('link');
-link.href =  chrome.extension.getURL('css/darkhub.css');
+link.href =  chrome.extension.getURL('css/darkhub.min.css');
 link.rel = 'stylesheet';
 document.documentElement.insertBefore(link);
