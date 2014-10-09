fae9823f1f5d55012bccadc59a0afa86aae25fb9
xero harrison <x@xero.nu>
2014-10-09 10:11:40 -0400

    fix autoload bug for chrome v38

--- js/start.js
+++ js/start.js
@@ -1,4 +1,5 @@
 var link = document.createElement('link');
 link.href =  chrome.extension.getURL('css/darkhub.min.css');
 link.rel = 'stylesheet';
-document.documentElement.insertBefore(link);
+head = document.getElementsByTagName ("head")[0] || document.documentElement;
+head.insertBefore(link, head.firstChild);
